6a580e6dc7fc3c2d045fe59ea3d582665806bf6c
srivalli <snc.srivalli1@gmail.com>
2022-11-06 22:17:25 +0530

    hghbgkj

--- src/test/java/SeleniumGrid/Test1.java
+++ src/test/java/SeleniumGrid/Test1.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class Test1{
 	@Test
 	public void gridPractice() throws MalformedURLException, InterruptedException {
-		
+		//abc
 		URL url = new URL("http://192.168.72.1:4444/wd/hub");
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setPlatform(Platform.WINDOWS);
